bff09e1315349a209c2eff2f024594c4897dd129
Ager <aghazofficial@gmail.com>
2026-04-20 19:47:19 +0800

    updated

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,20 @@
 document.addEventListener("mousemove", (e) => {
     const title = document.querySelector(".parallax-group");
-    
+
     const x = (window.innerWidth / 2 - e.pageX) / 60;
     const y = (window.innerHeight / 2 - e.pageY) / 60;
-    
+
     title.style.transition = 'transform 0.1s ease-out';
     title.style.transform = `translate(${x}px, ${y}px)`;
 });
 
 let isMoving = false;
 document.addEventListener("mousemove", () => {
-    if(!isMoving) {
+    if (!isMoving) {
         document.querySelector(".parallax-group").style.transition = 'none';
         isMoving = true;
     }
-    
+
     clearTimeout(window.moveTimeout);
     window.moveTimeout = setTimeout(() => {
         isMoving = false;
@@ -29,17 +29,17 @@ const createStars = () => {
     for (let i = 0; i < 80; i++) {
         let star = document.createElement("div");
         star.className = "star";
-        
+
         star.style.left = Math.random() * 100 + "vw";
         star.style.top = Math.random() * 100 + "vh";
-        
+
         const size = Math.random() * 2 + 1;
         star.style.width = size + "px";
         star.style.height = size + "px";
-        
+
         star.style.animationDelay = Math.random() * 3 + "s";
         star.style.animationDuration = Math.random() * 2 + 1.5 + "s";
-        
+
         starContainer.appendChild(star);
     }
 };
